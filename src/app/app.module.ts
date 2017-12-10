6710641bf13ffdfa5ab798ffd5461330b6827796
Naman Nanavati <naman.nanavati@gmail.com>
2017-12-10 13:55:26 +1100

    added pets component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {MatButtonModule, MatMenuModule, MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { DataService } from '../app/services/data.service';
+import { PetsComponent } from './components/pets/pets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PetsComponent
   ],
   imports: [
     BrowserModule,
